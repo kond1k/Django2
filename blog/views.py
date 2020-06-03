6e3ced7729a9e840dd4eaae25b9b07843743e0d6
kondik <kond1k12@gmail.com>
2020-06-03 16:18:47 +0300

    ad class

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.base import View
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("hi")
+
+
+class HomeView(View):
+    def get(self, request):
+        return HttpResponse("Good")
+
+    def post(self, request):
+        pass
