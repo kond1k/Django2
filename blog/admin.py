14db2680d68036b0b68d434c6efd99852a2a1e56
kondik <kond1k12@gmail.com>
2020-06-04 10:07:12 +0300

    add new models and create 3 post

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Tag, Post, Comment
 
 # Register your models here.
 
 admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Comment)
+
 
--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,40 @@ class Category(models.Model):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+
+
+class Tag(models.Model):
+    name = models.CharField(verbose_name='Имя', max_length=100)
+    slug = models.SlugField("url", max_length=100)
+
+    def __str__(self):
+        return(self.name)
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+
+
+class Post(models.Model):
+    title = models.CharField(verbose_name='Заголовок', max_length=100)
+    mini_text = models.TextField(verbose_name='Краткое содержание')
+    text = models.TextField(verbose_name='Текст')
+    created_date = models.DateTimeField(verbose_name='Время создания')
+    slug = models.SlugField(verbose_name='url', max_length=100)
+
+    def __str__(self):
+        return(self.title)
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+
+class Comment(models.Model):
+    text = models.TextField(verbose_name='Текст комментария')
+    created_date = models.DateTimeField(verbose_name='Время создания комментария')
+    moderation = models.BooleanField(verbose_name='Видимость коммнтария')
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Коментарии"
