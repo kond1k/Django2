886aebbedb4a9ff9c138f3e489f4191dd8308e92
kondik <kond1k12@gmail.com>
2020-06-11 08:45:23 +0300

    check

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 
+
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Block.settings')
     try:
@@ -13,6 +14,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
+            
         ) from exc
     execute_from_command_line(sys.argv)
 
